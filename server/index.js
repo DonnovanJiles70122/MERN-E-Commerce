971f3f9a648e59a2901f3362ff4af06827ed8ffd
Donnovan Jiles <djiles@mail.sfsu.edu>
2023-05-24 19:36:59 -0700

    backend

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connect successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api/test", () => {
+  console.log("test is successful");
+});
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
+});
